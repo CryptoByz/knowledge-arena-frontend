46260112349a46e5675504fc58372c4d453ac591
CryptoByz <cryptobyz.airdrop@gmail.com>
2026-06-13 11:04:39 +0300

    update ARC contract addresses

--- app/config/chains.ts
+++ app/config/chains.ts
@@ -39,13 +39,14 @@ export const SUPPORTED_CHAINS = [arcTestnet, baseSepolia] as const
 // Contract adresleri
 export const CONTRACTS = {
   [arcTestnet.id]: {
-    dailyQuiz:         '0x87dcE1Fc3ad5f31DFfFDc5F97A822B85EC059891',
-    playerProfile:     '0x20e461d0f4Fbe06E7bF6928277fb9143C5Da1cBA',
-    achievementManager:'0x85339D4078699466a4aAa4B2C7f89b4024a8Ba73',
-    rewardPool:        '0xBe0be37fF6018d211C63243018BC449d29f706E5',
-    badgeNFT:          '0xd88ADB6Ff6835535B97aff0765882D832c0a91F6',
-    usdc:              '0x3600000000000000000000000000000000000000',
+  dailyQuiz:          '0x2434e3b5713aA7631D4FE38bF28666e17E8639Da',
+  playerProfile:      '0x1EF12bB8C40BCE6eBD44b746f72B9291ED32A0B5',
+  achievementManager: '0x2f302dB1d2e979a038425da93CDC4Bbf25a0e590',
+  rewardPool:         '0x19D803932061E994AE7a4d48902061296af9f797',
+  badgeNFT:           '0x86359731BA0D3772CD95a63A093fF31d69E2250f',
+  usdc:               '0x3600000000000000000000000000000000000000',
   },
+
   [baseSepolia.id]: {
     dailyQuiz:         '0xd88ADB6Ff6835535B97aff0765882D832c0a91F6',
     playerProfile:     '0x5744f79D84Bf0d09284dAF780aE81485C5AaD70E',
